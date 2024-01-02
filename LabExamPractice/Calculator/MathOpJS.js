29c78dcbbe98ef82aa3b0c8b1a0a692db3ab8610
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-01-02 22:27:22 +0530

    added custom validation to input numbers

--- LabExamPractice/Calculator/MathOpJS.js
+++ LabExamPractice/Calculator/MathOpJS.js
@@ -1,8 +1,8 @@
 function MathOperation() {
   let x = parseInt(document.getElementById("num1").value);
   let y = parseInt(document.getElementById("num2").value);
+  ValidateNumber(x, y);
   let mathOp = document.getElementById("mathOp").value;
-  //   alert(mathOp);
   if (mathOp == "add") {
     document.getElementById("getResult").innerText = x + y;
   } else if (mathOp == "diff") {
@@ -11,5 +11,20 @@ function MathOperation() {
     document.getElementById("getResult").innerText = x * y;
   } else if (mathOp == "div") {
     document.getElementById("getResult").innerText = x / y;
+  }
+}
+function ValidateNumber(x, y) {
+  if (isNaN(x)) {
+    // alert("Enter Number!!");
+    document.getElementById("error1").innerHTML = "Please Enter Number";
+    document.getElementById("getResult").innerHTML = "";
+  }
+  if (isNaN(y)) {
+    // alert("Enter Number!!");
+    document.getElementById("error2").innerHTML = "Please Enter Number";
+    document.getElementById("getResult").innerHTML = "";
+  } else {
+    document.getElementById("error1").innerHTML = "";
+    document.getElementById("error2").innerHTML = "";
   }
 }
